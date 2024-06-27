6629f23ffb53ac6c483f44d77c796648c41702c7
dundar-davut-burak <dundardavutburak@gmail.com>
2024-06-27 12:45:04 +0300

    Create page.js

---
+++ src/app/hakkimda/page.js
@@ -0,0 +1,11 @@
+import About from "@/components/about";
+import Contact from "@/components/contact";
+
+export default function AboutPage() {
+  return (
+    <>
+      <About />
+      <Contact />
+    </>
+  );
+}
